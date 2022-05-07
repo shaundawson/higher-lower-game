a74058dacdc54f7bd0daa723fe6492f432e0ac2c
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-06 23:54:00 -0400

    map guess

--- main.py
+++ main.py
@@ -3,49 +3,45 @@ from art import vs
 from game_data import data
 import random
 
-print(logo)
+score = int(0)
 
-#Pick a random item from the dictionary. Print the name, description and location. 
-choice_a = random.choice(list(data))
-# print(choice_a)
-print(f"Compare A: {choice_a['name']}, a {choice_a['description']}, from {choice_a['country']}")  
-# Store follower count in a variable
-followers_a = (choice_a['follower_count'])
-print(f"Psst this person has {followers_a} followers")
-                 
-print(vs)
+def map_guess(followers_a, followers_b, guess):
+    if guess == "a" and followers_a > followers_b:
+        print("winner winner chicken dinner")
+        return score + 1
+    elif guess == "b" and followers_b > followers_a:
+        print("winner winner chicken dinner")
+        return score + 1
+    elif guess == followers_a and guess == followers_b:
+        print("it's a tie")
+    else: 
+        print("Sorry you lose.")
+        return
+  
 
-#Pick another random item from the dictionary. Print the name, description and location. 
-choice_b = random.choice(list(data))
-print(f"Compare B: {choice_b['name']}, a {choice_b['description']}, from {choice_b['country']}")  
-followers_b = (choice_b['follower_count'])
-print(f"Psst this person has {followers_b} followers")
+def game():
+ 
+    print(logo)
 
+    choice_a = random.choice(list(data))
+    print(f"Compare A: {choice_a['name']}, a {choice_a['description']}, from {choice_a['country']}")  
+    # Store follower count in a variable
+    followers_a = (choice_a['follower_count'])
+    print(f"Psst this person has {followers_a} followers")
 
-#Ask the user who has more followers. Who has more followers? Type 'A' or 'B': 
-guess = input("Who has more followers? Type 'A' or 'B': ")
+    print(vs)
 
-#If user is correct, give a point and continue.
+    #Pick another random item from the dictionary. Print the name, description and location. 
+    choice_b = random.choice(list(data))
+    print(f"Against B: {choice_b['name']}, a {choice_b['description']}, from {choice_b['country']}")  
+    followers_b = (choice_b['follower_count'])
+    
+    print(f"Psst this person has {followers_b} followers")
 
+    lower = min(followers_a, followers_b)
 
-# If user is correct, compare the winner of the comparison to a new random person.
+    guess = input('Who has more followers? Type "a" or "b": ')
+   
 
-#The game continues until the user guesses wrong
-
-#If the user is wrong, end the game. Sorry, that's wrong.  Final score: 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# print(f"Sorry, that's wrong.  Final score: {}")
-# print(f"You're right! Current score: {}")
+       
+game()
